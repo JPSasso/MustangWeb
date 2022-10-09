d3cbf0b7cfef73bab12647c190880fd0b01d0769
Juan Pablo Sasso <jp-sasso@hotmail.com>
2022-10-09 15:05:50 -0300

    añadido de boton para esconder sitemap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+function toggleSiteMapVisibility() {
+  var x = document.getElementById("siteMap");
+  var text = document.getElementById("siteMapToggleButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    text.innerHTML = 'Ver mas  <span>&#8743;</span>';
+  } 
+  else {
+    x.style.display = "none";
+    text.innerHTML = 'Ver mas  <span>&#8744;</span>';
+  }
+}
+
 function SubscribeButtonClick() {
     alert("Te has suscrito!");
-  }
+}
